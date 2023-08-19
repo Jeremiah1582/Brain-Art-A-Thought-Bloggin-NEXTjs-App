6936d53406fcc30f95c39bed214e29fd157c890b
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-08-19 14:07:36 +0200

    minor changes to wording of title

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       {/* <div>Home</div> */}
       <h1 className='head_text text-center'>
-        discover & share
+        Discover & Share
         <br className="max-md:hidden" />
-        <span className="purple_gradient"> Whats on your mind</span>
+        <span className="purple_gradient"> Your Thoughts </span>
       </h1>
       <p className="desc text-center">A blogging platform for people that want to share a short burst of creativity</p>
 {/* feed */}
